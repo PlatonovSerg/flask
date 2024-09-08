b508b2622ce4672605ce5f0a8eb9e0f7aa10e72b
PlatonovSerg <platonov1727@gmail.com>
2024-09-08 17:52:55 +0300

    add ExceptionHandlers to API

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from pathlib import Path
+
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask
 from settings import Config
-from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
 app = Flask(__name__, instance_path=BASE_DIR / "instance")
 app.config.from_object(Config)
+app.config["JSON_AS_ASCII"] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import cli_commands, error_handlers, views
+from . import api_views, cli_commands, error_handlers, views
