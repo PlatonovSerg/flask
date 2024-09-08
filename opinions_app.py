4f6f72441ec3c84c47ea1c0ea74528f26a119f43
PlatonovSerg <platonov1727@gmail.com>
2024-09-08 15:38:44 +0300

    add 404, 500, install Flask-Migrate

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -9,8 +9,8 @@ from wtforms.validators import DataRequired, Length, Optional
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "GhjcnjNfrNsYbxtujYtLj,mtimcz"
 
 db = SQLAlchemy(app)
@@ -26,50 +26,63 @@ class Opinion(db.Model):
 
 class OpinionForm(FlaskForm):
     title = StringField(
-        'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(1, 128)]
+        "Введите название фильма",
+        validators=[DataRequired(message="Обязательное поле"), Length(1, 128)],
     )
     text = TextAreaField(
-        'Напишите мнение', 
-        validators=[DataRequired(message='Обязательное поле')]
+        "Напишите мнение",
+        validators=[DataRequired(message="Обязательное поле")],
     )
     source = URLField(
-        'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()]
+        "Добавьте ссылку на подробный обзор фильма",
+        validators=[Length(1, 256), Optional()],
     )
-    submit = SubmitField('Добавить')
+    submit = SubmitField("Добавить")
 
 
-@app.route('/')
+@app.route("/")
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных записей нет.'
+        abort(404)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
-    return render_template('opinion.html', opinion=opinion)
-    
+    return render_template("opinion.html", opinion=opinion)
 
-@app.route('/add', methods=['GET', 'POST'])
+
+@app.route("/add", methods=["GET", "POST"])
 def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash("Такое мнение уже было оставлено ранее!")
+            return render_template("add_opinion.html", form=form)
         opinion = Opinion(
-            title=form.title.data,
-            text=form.text.data,
-            source=form.source.data
+            title=form.title.data, text=form.text.data, source=form.source.data
         )
         db.session.add(opinion)
         db.session.commit()
-        return redirect(url_for('opinion_view', id=opinion.id))
-    return render_template('add_opinion.html', form=form)
+        return redirect(url_for("opinion_view", id=opinion.id))
+    return render_template("add_opinion.html", form=form)
 
 
-@app.route('/opinions/<int:id>')  
-def opinion_view(id):  
-    opinion = Opinion.query.get_or_404(id)  
-    return render_template('opinion.html', opinion=opinion)
+@app.route("/opinions/<int:id>")
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template("opinion.html", opinion=opinion)
 
-if __name__ == '__main__':
-    app.run() 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template("500.html"), 500
+
+
+if __name__ == "__main__":
+    app.run()
