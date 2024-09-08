8ac5244e3c5a6e8b630d6fb4fd4ef3074f72ee0e
PlatonovSerg <platonov1727@gmail.com>
2024-09-08 16:52:48 +0300

    fix BASE_DIR of instance with DB add to __init__.py BASE_DIR = Path(__file__).parent app = Flask(__name__, instance_path=BASE_DIR / 'instance")

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from settings import Config
+from pathlib import Path
 
-app = Flask(__name__)
+BASE_DIR = Path(__file__).parent
+
+app = Flask(__name__, instance_path=BASE_DIR / "instance")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
